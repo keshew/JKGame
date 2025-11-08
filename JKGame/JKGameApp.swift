13ffb67c6585c64fdc2ace3250fcabca658283f5
keshew <artyom.korotckov2017@yandex.ru>
2025-11-08 14:02:00 +0500

    Initial Commit

---
+++ JKGame/JKGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  JKGameApp.swift
+//  JKGame
+//
+//  Created by Артём Коротков on 08.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct JKGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
